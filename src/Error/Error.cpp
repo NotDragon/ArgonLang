308798059b6931bdcbac83cd990ad2b1e85a7c13
NotDragon <mixalis.m.hadjittofi@gmail.com>
2024-12-27 15:41:43 +0200

    Removed Error.h

--- src/Error/Error.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by User on 14/12/2024.
-//
-#include "Error/Error.h"
-
-using namespace ArgonLang;
-
-Error::Error(std::string errorMsg, Type type): errorMsg(std::move(errorMsg)), errorType(type) {}
